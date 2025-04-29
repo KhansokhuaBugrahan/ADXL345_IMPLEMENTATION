b1c7ef098119140410633411c0e4855baa5d54d6
KhansokhuaBugrahan <bugrahan.barisik@gmail.com>
2025-04-29 18:00:20 +0300

    Update adxl345.h
    Alignment of code lines

--- adxl345.h
+++ adxl345.h
@@ -13,35 +13,35 @@
 #include <spi.h>
 // Register MAP
 
-#define			DEVID_BASE_ADDR			 (0x00) //  Device ID
+#define			DEVID_BASE_ADDR		 (0x00) //  Device ID
 #define			THRESH_TAP_BASE_ADDR	 (0x1D) //  Tap threshold
-#define			OFSX_BASE_ADDR			 (0x1E) //  X-axis offset
-#define			OFSY_BASE_ADDR			 (0x1F) //  Y-axis offset
-#define			OFSZ_BASE_ADDR		     (0x20) //  Z-axis offset
-#define			DUR_BASE_ADDR			 (0x21) //  Tap duration
-#define			LATENT_BASE_ADDR		 (0x22) //  Tap latency
-#define			Window_BASE_ADDR		 (0x23) //  Tap window
+#define			OFSX_BASE_ADDR		 (0x1E) //  X-axis offset
+#define			OFSY_BASE_ADDR		 (0x1F) //  Y-axis offset
+#define			OFSZ_BASE_ADDR		 (0x20) //  Z-axis offset
+#define			DUR_BASE_ADDR		 (0x21) //  Tap duration
+#define			LATENT_BASE_ADDR	 (0x22) //  Tap latency
+#define			Window_BASE_ADDR	 (0x23) //  Tap window
 #define			THRESH_ACT_BASE_ADDR	 (0x24)	//  Activity threshold
 #define			THRESH_INACT_BASE_ADDR	 (0x25) //  Inactivity threshold
 #define			TIME_INACT_BASE_ADDR	 (0x26) //  Inactivity time
 #define			ACT_INACT_CTL_BASE_ADDR	 (0x27) //  Axis enable control for activity and inactivity detection
-#define			THRESH_FF_BASE_ADDR		 (0x28)	//  Free-fall threshold
-#define			TIME_FF_BASE_ADDR		 (0x29)	//  Free-fall time
-#define			TAP_AXES_BASE_ADDR		 (0x2A)	//  Axis control for single tap/double tap
+#define			THRESH_FF_BASE_ADDR	 (0x28)	//  Free-fall threshold
+#define			TIME_FF_BASE_ADDR	 (0x29)	//  Free-fall time
+#define			TAP_AXES_BASE_ADDR	 (0x2A)	//  Axis control for single tap/double tap
 #define			ACT_TAP_STATUS_BASE_ADDR (0x2B)	//  Source of single tap/double tap
-#define			BW_RATE_BASE_ADDR		 (0x2C) //  Data rate and power mode control
-#define			POWER_CTL_BASE_ADDR		 (0x2D) //  Power-saving features control
+#define			BW_RATE_BASE_ADDR	 (0x2C) //  Data rate and power mode control
+#define			POWER_CTL_BASE_ADDR	 (0x2D) //  Power-saving features control
 #define			INT_ENABLE_BASE_ADDR	 (0x2E)	//  Interrupt enable control
-#define			INT_MAP_BASE_ADDR		 (0x2F)	//  Interrupt mapping control
+#define			INT_MAP_BASE_ADDR	 (0x2F)	//  Interrupt mapping control
 #define			INT_SOURCE_BASE_ADDR	 (0x30)	//  Source of interrupts
 #define			DATA_FORMAT_BASE_ADDR	 (0x31) //  Data format control
-#define			DATAX0_BASE_ADDR		 (0x32) //  X-Axis Data 0
-#define			DATAX1_BASE_ADDR		 (0x33)	//  X-Axis Data 1
-#define			DATAY0_BASE_ADDR		 (0x34) //  Y-Axis Data 0
-#define			DATAY1_BASE_ADDR		 (0x35) //  Y-Axis Data 1
-#define			DATAZ0_BASE_ADDR		 (0x36) //  Z-Axis Data 0
-#define			DATAZ1_BASE_ADDR	 	 (0x37)	//  Z-Axis Data 1
-#define			FIFO_CTL_BASE_ADDR		 (0x38) //  FIFO control
+#define			DATAX0_BASE_ADDR	 (0x32) //  X-Axis Data 0
+#define			DATAX1_BASE_ADDR	 (0x33)	//  X-Axis Data 1
+#define			DATAY0_BASE_ADDR	 (0x34) //  Y-Axis Data 0
+#define			DATAY1_BASE_ADDR	 (0x35) //  Y-Axis Data 1
+#define			DATAZ0_BASE_ADDR	 (0x36) //  Z-Axis Data 0
+#define			DATAZ1_BASE_ADDR	 (0x37)	//  Z-Axis Data 1
+#define			FIFO_CTL_BASE_ADDR	 (0x38) //  FIFO control
 #define			FIFO_STATUS_BASE_ADDR	 (0x39) //  FIFO status
 
 /*
